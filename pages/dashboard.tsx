33c086d46f0b116c0b6f37a66b56c96c6962fd7f
Gabe Kirkley <gdgkirkley@gmail.com>
2021-05-15 10:20:57 -0700

    Add minimal page flow

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,15 @@
 import { UserProfile, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
 
 const Dashboard = ({ user }: { user: UserProfile }): React.ReactElement => {
-  return <p>Welcome, {user.name}!</p>;
+  return (
+    <div>
+      <p>Welcome, {user.name}!</p>
+      <p>
+        <Link href="/">Return home</Link>
+      </p>
+    </div>
+  );
 };
 
 export const getServerSideProps = withPageAuthRequired();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-const HomePage = (): React.ReactElement => <h1>Welcome to The Round!</h1>;
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
+
+const HomePage = (): React.ReactElement => {
+  const { user } = useUser();
+
+  console.log(user);
+
+  return (
+    <div>
+      <h1>Welcome to The Round!</h1>
+      {!user && <Link href="/api/auth/login">Login</Link>}
+      {user && (
+        <ul>
+          <li>
+            <Link href="/dashboard">Go to dashboard</Link>
+          </li>
+          <li>
+            <Link href="/api/auth/logout">Logout</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
